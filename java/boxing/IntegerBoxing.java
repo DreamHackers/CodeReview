5089ff799bf5547d68bdc5a1e431d23bebc87bf0
Gaddict <ksano6253@gmail.com>
2016-01-27 15:04:44 +0900

    [Java][Boxing]parseIntに変更

--- java/boxing/IntegerBoxing.java
+++ java/boxing/IntegerBoxing.java
@@ -1,4 +1,4 @@
 // エラーが出ないからOKという考え方をやめ、型をしっかり考えた方が良い
 
-        final int sysCdCount = Integer.valueOf(m.get("UNIQUE_SYSTEM_CD_COUNT").toString());
-        final int dcfCdCount = Integer.valueOf(m.get("UNIQUE_DCF_DR_CD_COUNT").toString());
+        final int sysCdCount = Integer.parseInt(m.get("UNIQUE_SYSTEM_CD_COUNT").toString());
+        final int dcfCdCount = Integer.parseInt(m.get("UNIQUE_DCF_DR_CD_COUNT").toString());
