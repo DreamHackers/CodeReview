2738cb51f23b5d6aac7290a90177eaf710f6ba6d
Gaddict <ksano6253@gmail.com>
2016-01-27 14:15:51 +0900

    IntegerとBoxing

---
+++ java/boxing/IntegerBoxing.java
@@ -0,0 +1,4 @@
+// エラーが出ないからOKという考え方をやめ、型をしっかり考えた方が良い
+
+        final int sysCdCount = Integer.valueOf(m.get("UNIQUE_SYSTEM_CD_COUNT").toString());
+        final int dcfCdCount = Integer.valueOf(m.get("UNIQUE_DCF_DR_CD_COUNT").toString());
